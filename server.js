be789d02cdc89d6b1447d46badac056bef9691ff
Young <rhsiddlsidhd@nate.com>
2025-05-21 11:15:49 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewear = jsonServer.defaults();
+const router = jsonServer.router("db.json");
+const express = require("express");
+const port = 8000;
+server.use(express.json());
+server.use(middlewear);
+
+server.use(router);
+
+server.listen(port, (err) => {
+  if (err) console.log(err);
+  console.log(`Json server Open : http://localhost:${port}`);
+});
